2a9aad1a225e031c83d09f31d0e405aaede63397
svc-student <svc-student@student.directory.jccc>
2018-09-04 18:15:42 -0500

    Dijkstra/A* Progress

---
+++ Dijkstra/Assets/Scripts/Node.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Node
+{
+    public Vector3 nodePosition;
+    public float nodeWeight;
+    public bool nodeBlocked;
+    public List<Node> connectedNodes = new List<Node>();
+    public int numConnectedNodes = 0;
+
+    public Node(Vector3 position, float weight, bool blocked)
+    {
+        nodePosition = position;
+        nodeWeight = weight;
+        nodeBlocked = blocked;
+    }
+}
---
+++ Dijkstra/Assets/Scripts/Pathfinding.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Pathfinding : MonoBehaviour
+{
+    FloorGrid grid;
+
+	// Use this for initialization
+	void Start ()
+    {
+        grid = GetComponent<FloorGrid>();
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+		
+	}
+
+    private void FindPath(Vector3 startPosition, Vector3 targetPosition)
+    {
+        Node startNode = grid.ConvertToGridPosition(startPosition);
+        Node targetNode = grid.ConvertToGridPosition(targetPosition);
+
+        List<Node> openNodes = new List<Node>();
+        List<Node> closedNodes = new List<Node>();
+        openNodes.Add(startNode);
+
+        //Next: start figuring out the path to the target
+        
+    }
+}
