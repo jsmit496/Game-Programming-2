3623864c06863d4f82776c4928fe009a8b024678
svc-student <svc-student@student.directory.jccc>
2018-09-20 18:18:23 -0500

    network stuff

---
+++ MultiplayerNetwork/Assets/Scripts/NetworkHandler.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.Networking;
+
+public class NetworkHandler : MonoBehaviour
+{
+    public Text joinGameText;
+    public Text playerNameText;
+
+	// Use this for initialization
+	void Start ()
+    {
+		
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+		
+	}
+
+    public void HostGame()
+    {
+        GetComponent<NetworkManager>().StartHost();
+    }
+
+    public void JoinGame()
+    {
+        GetComponent<NetworkManager>().networkAddress = joinGameText.text;
+        GetComponent<NetworkManager>().StartClient();
+    }
+}
