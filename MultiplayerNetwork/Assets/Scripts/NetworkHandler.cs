6f9158c2b3d43a6df84425882f5196adc02467cb
svc-student <svc-student@student.directory.jccc>
2018-09-25 18:20:14 -0500

    network name functionality

--- MultiplayerNetwork/Assets/Scripts/NetworkHandler.cs
+++ MultiplayerNetwork/Assets/Scripts/NetworkHandler.cs
@@ -3,11 +3,16 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 using UnityEngine.Networking;
+using UnityEngine.Networking.NetworkSystem;
 
-public class NetworkHandler : MonoBehaviour
+public class NetworkHandler : NetworkManager
 {
+    public string playerName;
+
     public Text joinGameText;
     public Text playerNameText;
+
+    //add display place for chat
 
 	// Use this for initialization
 	void Start ()
@@ -18,17 +23,39 @@ public class NetworkHandler : MonoBehaviour
 	// Update is called once per frame
 	void Update ()
     {
-		
+
 	}
 
-    public void HostGame()
+    public void SetupHost()
     {
-        GetComponent<NetworkManager>().StartHost();
+        StartHost();
+        NetworkServer.RegisterHandler(2001, OnPlayerNameRecieved);
+        client.RegisterHandler(2004, OnOtherPlayerJoinedGame);
     }
 
-    public void JoinGame()
+    public void SetupClient()
     {
-        GetComponent<NetworkManager>().networkAddress = joinGameText.text;
-        GetComponent<NetworkManager>().StartClient();
+        networkAddress = joinGameText.text;
+        StartClient();
+        client.RegisterHandler(2003, OnOtherPlayerJoinedGame);
+    }
+
+    public override void OnClientConnect(NetworkConnection netConnect)
+    {
+        //When client connects: set the chat
+        client.Send(2001, new StringMessage(playerName));
+    }
+
+    public void OnOtherPlayerJoinedGame(NetworkMessage netMsg)
+    {
+        string playerName = netMsg.ReadMessage<StringMessage>().value;
+        print(playerName + "joined");
+        //display name in chat
+    }
+
+    public void OnPlayerNameRecieved(NetworkMessage netMsg)
+    {
+        string playerName = netMsg.ReadMessage<StringMessage>().value;
+        NetworkServer.SendToAll(2003, new StringMessage(playerName));
     }
 }
