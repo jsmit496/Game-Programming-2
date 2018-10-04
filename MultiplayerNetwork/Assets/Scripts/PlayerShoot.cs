85d53f2c85db027989431446abb8e6e1d8dac5f4
svc-student <svc-student@student.directory.jccc>
2018-10-04 18:10:32 -0500

    Worked on shooting for Project2

---
+++ MultiplayerNetwork/Assets/Scripts/PlayerShoot.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Networking;
+
+public class PlayerShoot : NetworkBehaviour
+{
+    public float bulletSpeed = 4.0f;
+    public float bulletSpreadAngle;
+    public bool canAimDownSight = true;
+
+    public Transform shootPositon;
+    public GameObject bullet;
+
+	// Use this for initialization
+	void Start ()
+    {
+		
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+		if (Input.GetKeyDown(KeyCode.Mouse0))
+        {
+            CmdShoot();
+        }
+        
+	}
+
+    [Command]
+    void CmdShoot()
+    {
+        GameObject dummyBullet = Instantiate(bullet, shootPositon.position, shootPositon.rotation);
+        dummyBullet.GetComponent<Rigidbody>().AddForce(dummyBullet.transform.forward * bulletSpeed);
+
+        NetworkServer.Spawn(dummyBullet);
+    }
+}
---
+++ MultiplayerNetwork/Assets/Scripts/ShootingLesson.cs
@@ -0,0 +1,64 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Networking;
+
+public class ShootingLesson : NetworkBehaviour
+{
+    public Transform shootPosition;
+    public GameObject bullet;
+    public float launchForce = 100;
+
+    public int numberOfShotsFired;
+
+	// Use this for initialization
+	void Start ()
+    {
+		
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+		if (Input.GetButtonDown("Fire1"))
+        {
+            CmdFire();
+        }
+	}
+
+    //Only server executes the code when useing [Command], also needs "Cmd" prefix
+    [Command]
+    void CmdFire()
+    {
+        //Only server will see this number increase
+        numberOfShotsFired++;
+
+        GameObject newBullet = GameObject.Instantiate(bullet, shootPosition.position, shootPosition.rotation);
+        newBullet.GetComponent<Rigidbody>().AddForce(newBullet.transform.forward * launchForce);
+
+        //Handle spawning on each client
+        //In order to spawn it needs to be a prefab,have network identity, and add to registered spawnable prefabs on networkHandler
+        NetworkServer.Spawn(newBullet);
+    }
+
+    //Will execute on the client
+    [ClientRpc]
+    void RpcShowHit()
+    {
+        print("I got hit!");
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        //bool for if it is active on the server (if not active on server then do nothing)
+        if (!isServer)
+        {
+            return;
+        }
+
+        if (collision.gameObject.tag == "Bullet")
+        {
+            RpcShowHit();
+        }
+    }
+}
