ac197d7dad0c0606ee87ffa92ef8223249848a3f
svc-student <svc-student@student.directory.jccc>
2018-11-15 18:16:10 -0600

    RoomGenerator

---
+++ RoomGenerator/Assets/Node.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Node : MonoBehaviour
+{
+    public bool isRoom;
+    public int gridX, gridY;
+
+    public Vector2 position;
+
+    public Node(bool _isRoom, Vector3 _position, int _gridX, int _gridY)
+    {
+        isRoom = _isRoom;
+        position = _position;
+        gridX = _gridX;
+        gridY = _gridY;
+    }
+}
