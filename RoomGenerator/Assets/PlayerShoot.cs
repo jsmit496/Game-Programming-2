116a3f5da22c9a4c46c9e766d7303f169f41f744
jsmit496 <jsmit496@stumail.jccc.edu>
2018-12-06 15:00:00 -0600

    stuff

---
+++ RoomGenerator/Assets/PlayerShoot.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerShoot : MonoBehaviour
+{
+    public GameObject bullet;
+    public Transform shootPosition;
+    public float bulletSpeed;
+
+
+	// Use this for initialization
+	void Start ()
+    {
+		
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        if (Input.GetKeyDown(KeyCode.Mouse0))
+        {
+            Shoot();
+        }
+    }
+
+    void Shoot()
+    {
+        GameObject dummyBullet;
+        dummyBullet = Instantiate(bullet, shootPosition.position, shootPosition.rotation);
+        dummyBullet.GetComponent<Rigidbody>().AddForce(dummyBullet.transform.forward * bulletSpeed * Time.deltaTime);
+    }
+}
